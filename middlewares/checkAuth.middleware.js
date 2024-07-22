831a92bca24e9fd39afff2e314a2f200adb5f9b8
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-23 04:56:25 +0530

    added auth check middleware, only allows req to pass throw if it's authenticated

---
+++ middlewares/checkAuth.middleware.js
@@ -0,0 +1,22 @@
+const { verifyToken } = require("../utility/authToken.util");
+const throwError = require("../utility/throwError");
+
+const checkAuth = (req, res, next) => {
+  const authScheme = "Bearer";
+  const authorization = req.get("authorization");
+  if (!authorization) {
+    return throwError(400, "login required");
+  }
+  if (!authorizaton.startsWith(authScheme)) {
+    return throwError(400, "invalid auth scheme");
+  }
+
+  const token = authorization.slice(authScheme.length + 1);
+  const user = verifyToken(token);
+  req.user = user;
+  next();
+};
+
+module.exports = {
+  checkAuth,
+};

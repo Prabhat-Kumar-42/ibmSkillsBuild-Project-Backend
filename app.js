5869b92398e26c27ed42b8a6c8acb50038cf00f9
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-23 04:56:46 +0530

    added error handlers to app.js

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const express = require("express");
 require("express-async-errors");
 const app = express();
 const cors = require("cors");
+const {
+  unknownEndpoint,
+  mongoError,
+  jwtError,
+  errorHandler,
+} = require("./middlewares/errorHandlers.middleware");
+
+// Error Handler
+app.use(unknownEndpoint);
+app.use(mongoError);
+app.use(jwtError);
+app.use(errorHandler);
 
 module.exports = {
   app,

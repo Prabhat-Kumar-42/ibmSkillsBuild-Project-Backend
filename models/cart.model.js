5506f1c6d159c46130c7b1cbf084f0cf623874b3
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-25 00:01:20 +0530

    added json transform to cart model

--- models/cart.model.js
+++ models/cart.model.js
@@ -10,8 +10,17 @@ const cartSchema = {
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
+    unique: true,
   },
 };
+
+cartSchema.set("toJSON", {
+  transform: (document, returnObj) => {
+    returnObj.id = returnObj._id.toString();
+    delete returnObj._id;
+    delete returnObj.__v;
+  },
+});
 
 const Cart = mongoose.model("Cart", cartSchema);
 

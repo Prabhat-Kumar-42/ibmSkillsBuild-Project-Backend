46a9561c4d1f9d9a768beeaac017468e8f3bed8c
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-24 19:18:39 +0530

    added json transform to itemSchema

--- models/item.model.js
+++ models/item.model.js
@@ -52,6 +52,15 @@ const itemSchema = new mongoose.Schema({
   },
 });
 
+itemSchema.set("toJSON", {
+  transform: (document, returnObj) => {
+    returnObj.id = returnObj._id.toString();
+    returnObj.shopId = returnObj.shopId.toString();
+    delete returnObj._id;
+    delete returnObj.__v;
+  },
+});
+
 const Item = mongoose.model("Item", itemSchema);
 
 module.exports = {

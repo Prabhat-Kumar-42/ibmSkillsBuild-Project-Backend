eb71bab0cc9f95e2aaadc6c6f907fc7170733b9c
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-25 00:25:50 +0530

    added cart router

---
+++ router/api/cart.route.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cartRouter = express.Router();
+
+const { checkAuth } = require("../../middlewares/checkAuth.middleware");
+const {
+  handleGetCart,
+  handleUpdateCart,
+  handleDeleteCart,
+} = require("../../controllers/api/cart.controller");
+
+cartRouter
+  .route("/")
+  .get(checkAuth, handleGetCart)
+  .put(checkAuth, handleUpdateCart)
+  .delete(checkAuth, handleDeleteCart);
+
+cartRouter.route("/cartDetails").get();

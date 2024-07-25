f1d562037d82e10a12df2146cc040570d7c58399
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-25 16:50:07 +0530

    added discounts fields to sample item data

--- tests/testUtilities/mockData/itemSampleData.js
+++ tests/testUtilities/mockData/itemSampleData.js
@@ -4,30 +4,35 @@ const itemSampleData = [
     category: "Grocery",
     price: 2.99,
     quantity: 50,
+    discount: 5,
   },
   {
     name: "Cotton T-Shirt",
     category: "Clothing",
     price: 15.99,
     quantity: 100,
+    discount: 10,
   },
   {
     name: "Bluetooth Earphones",
     category: "Electronics",
     price: 49.99,
     quantity: 30,
+    discount: 50,
   },
   {
     name: "The Great Gatsby",
     category: "Books",
     price: 12.49,
     quantity: 20,
+    discount: 25,
   },
   {
     name: "Coffee Table",
     category: "Furniture",
     price: 149.99,
     quantity: 10,
+    discount: 100,
   },
 ];
 
